f36b017fe7ff67def30fa1ae3f18227577d84b76
mikoavelli <113817627+mikoavelli@users.noreply.github.com>
2024-05-20 21:42:18 +0300

    Delete lab4 directory

--- lab4/app.py
+++
@@ -1,9 +0,0 @@
-import os
-print("\n1. Run in CLI\n2. Run in WEB")
-
-while True:
-    choice = input("Enter your choice: ")
-    if choice == '1':
-        os.system('python lab1.py')
-    elif choice == '2':
-        os.system('python lab4.py')
